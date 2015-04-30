e496069312303e8b35f154c5d6fbcf620a90cbd8
Adam Morris <adam@kernelpanicstudios.com>
2015-04-29 22:39:06 -0500

    Got the CMakeLists file set up and added an empty entry point.

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Morris on 4/29/15.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Adam Morris on 4/29/15.
+//
+
+#ifndef CITYSCAPE_BUILDER_MAIN_H
+#define CITYSCAPE_BUILDER_MAIN_H
+
+#endif //CITYSCAPE_BUILDER_MAIN_H
